02482f75d6eedd4ca7b571b6babe943e727e6e7e
unknown <farizashakir13@gmail.com>
2020-11-12 19:46:01 +0530

    styling

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -9,7 +9,10 @@ class Pokecard extends Component{
         let imgsrc=`${POKE_API}${padToThree(this.props.id)}.png`
       return ( <div className="Pokecard">
       <h1 className="Pokecard-title"> {this.props.name}</h1>
-            <img src={imgsrc} alt={this.props.name}/> 
+      <div className="Pokecard-image">
+      <img src={imgsrc} alt={this.props.name}/> 
+      </div>
+           
     <div className="Pokecard-data">Type: {this.props.type}</div> 
     <div className="Pokecard-data">EXP: {this.props.exp}</div>  
         </div>
