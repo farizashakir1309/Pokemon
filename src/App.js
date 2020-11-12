03e57df1deb668fc1e3694a003294d4eb7759e68
unknown <farizashakir13@gmail.com>
2020-11-12 19:11:26 +0530

    create pokegame

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import './App.css';
-import Pokedex from './Pokedex.js';
+import Pokegame from './Pokegame.js';
 function App() {
   return (
     <div className="App">
-      < Pokedex />
+      < Pokegame />
     </div>
   );
 }
